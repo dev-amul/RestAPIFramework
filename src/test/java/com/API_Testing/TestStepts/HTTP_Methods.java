c39ba3f5d3d52fc421dad362770b385a072762fa
dev-amul <amulc451@gmail.com>
2019-09-15 21:35:29 +0530

    my rest assured second pproject

---
+++ src/test/java/com/API_Testing/TestStepts/HTTP_Methods.java
@@ -0,0 +1,79 @@
+/**
+ * @author 			:	 sumitkumar
+ *	DATE       		:	 15-Sep-2019
+ *  FILE NAME  		: 	 HTTP_Methods.java
+ *  PROJECT NAME 	:	 RestAPI_Framework
+ * 
+ */
+package com.API_Testing.TestStepts;
+
+import java.util.Properties;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+
+public class HTTP_Methods 
+{
+	Properties pr; 
+	public HTTP_Methods(Properties pr) {
+		
+		this.pr=pr;
+	}
+	
+	public Response post_Request(String dataForPost, String postOnURI) 
+	{
+		Response postRequestResponse = 
+				given()
+				.contentType(ContentType.JSON)
+				.body(dataForPost)
+				.when()
+				.post(pr.getProperty(postOnURI));
+		
+		return postRequestResponse;
+		
+	}
+	public Response get_Request(String getFromURI) {
+		
+		Response fetchDataFromURI = 
+				
+				given()
+				.contentType(ContentType.JSON)
+				.when()
+				.get(pr.getProperty(getFromURI));
+				
+				return fetchDataFromURI;
+	}
+	public Response put_Request(String payLoad, String uriForPut) {
+		
+		Response putRequest_Respose = 
+				
+				given()
+				.contentType(ContentType.JSON)
+				.body(payLoad)
+				.when()
+				.put(pr.getProperty(uriForPut));
+				
+				
+				return putRequest_Respose;
+	}
+	public Response patch_Request(String existingData, String updateOnURI) {
+		
+		Response patchRequestResponse = 
+				
+				given()
+				.contentType(ContentType.JSON)
+				.body(existingData)
+				.when()
+				.patch(pr.getProperty(updateOnURI));
+		
+		return patchRequestResponse; 
+		
+	}
+	
+	public void delete_Request() {
+		
+	}
+
+}
---
+++ src/test/java/com/API_Testing/utilities/Laod_PropertiestFile.java
@@ -0,0 +1,32 @@
+/**
+ * @author 			:	 sumitkumar
+ *	DATE       		:	 15-Sep-2019
+ *  FILE NAME  		: 	 Laod_PropertiestFile.java
+ *  PROJECT NAME 	:	 RestAPI_Framework
+ * 
+ */
+package com.API_Testing.utilities;
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+
+public class Laod_PropertiestFile 
+{
+	public static Properties getPropertyFile() throws IOException
+	{
+		
+		FileInputStream getFile = new FileInputStream("../RestAPI_Framework/Environment_URI.properties");
+		
+		Properties pr = new Properties();
+		
+		pr.load(getFile);
+		
+		return pr;
+	}
+	
+	
+
+}
