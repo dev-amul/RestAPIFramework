5464effa04b08bb8c75965fbdcb328b8aaed848d
dev-amul <amulc451@gmail.com>
2019-10-06 23:54:30 +0530

    add tc 6 for fetch and add object

---
+++ src/test/java/com/API_Testing/TestScripts/TC_06_AddValueInExsitingData.java
@@ -0,0 +1,33 @@
+/**
+ * @author 			:	 sumitkumar
+ *	DATE       		:	 06-Oct-2019
+ *  FILE NAME  		: 	 TC_06_AddValueInExsitingData.java
+ *  PROJECT NAME 	:	 RestAPI_Framework
+ * 
+ */
+package com.API_Testing.TestScripts;
+
+import java.io.FileNotFoundException;
+
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+import com.API_Testing.Resources.AlltypeDataRead;
+import com.API_Testing.Resources.UpdateExitingJSON;
+
+public class TC_06_AddValueInExsitingData 
+{
+	@Test
+	public void addValueExsitingData() throws FileNotFoundException {
+		
+		JSONObject jsondummydata= AlltypeDataRead.readJsonFile("../RestAPI_Framework/DataFile/jsonBodydumm.json");
+		
+		String str= UpdateExitingJSON.fetchAndUdateData(jsondummydata.toString(), "phoneNumbers", "type", "home", "emailId", "dummy@gmai.com");
+		
+		System.out.println("#######Expected Data###########\n");
+		
+		System.out.println(str);
+		
+		
+	}
+
+}
