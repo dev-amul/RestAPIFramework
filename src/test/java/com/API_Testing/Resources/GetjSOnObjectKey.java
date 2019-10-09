6e11d89ee8d1a514235b73b1bfdd8e8898223363
dev-amul <amulc451@gmail.com>
2019-10-09 09:01:34 +0530

    get keys from the json body and replace the variable to values

--- src/test/java/com/API_Testing/Resources/GetjSOnObjectKey.java
+++ src/test/java/com/API_Testing/Resources/GetjSOnObjectKey.java
@@ -9,6 +9,9 @@ package com.API_Testing.Resources;
 
 import java.io.FileNotFoundException;
 import java.util.Iterator;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 import org.json.JSONObject;
 public class GetjSOnObjectKey
 {
@@ -17,14 +20,26 @@ public class GetjSOnObjectKey
 		// Call the jSon file reader method 
 		JSONObject storeJsonobj= AlltypeDataRead.readJsonFile("../RestAPI_Framework/DataFile/dynamicVariableBody.json");		
 		
-		Iterator arr= storeJsonobj.keys();
+		Iterator getjSOnKeys= storeJsonobj.keys();
 		
-		String s=null;
-		while(arr.hasNext()) 
+		String jsonBody = storeJsonobj.toString();
+		
+		String s3=null;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter replacement values as correspondence to variables");
+		
+		while(getjSOnKeys.hasNext()) 
 		{
-			s=arr.next().toString();
-			System.out.println(s);
-		}	
-		 //return obj;
+			String jSonKeys=getjSOnKeys.next().toString();
+			String getVariables=storeJsonobj.getString(jSonKeys).toString();
+		//	System.out.println(s1);
+			System.out.print(jSonKeys+"\t:\t");			
+			String replacement=sc.next();
+			jsonBody=jsonBody.replaceAll(Pattern.quote(getVariables), replacement);
+			s3=jsonBody;
+		}
+		System.out.println("********** Json before replace the variable ********** \n"+storeJsonobj.toString());
+		System.out.println("********** json after replace the variable  ********** \n"+s3);
 	}
 }
