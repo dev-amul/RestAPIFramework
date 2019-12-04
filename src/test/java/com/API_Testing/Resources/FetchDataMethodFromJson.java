835603e5734cd8d3c04b9168adf24c2b5c367dd2
dev-amul <amulc451@gmail.com>
2019-12-04 09:58:04 +0530

    get value from different json

--- src/test/java/com/API_Testing/Resources/FetchDataMethodFromJson.java
+++ src/test/java/com/API_Testing/Resources/FetchDataMethodFromJson.java
@@ -16,32 +16,36 @@ import org.json.JSONObject;
 public class FetchDataMethodFromJson 
 
 {
-	public static void main(String[] args) throws FileNotFoundException
+	public static String getValueFromJson(String srhObj, String srhArray, String srchArray, String srchKey, String srchValu, String gtExtData) throws FileNotFoundException
 	{
 		JSONObject jsonBody = AlltypeDataRead.readJsonFile("../RestAPI_Framework/DataFile/CoremrkData.json");
-		
-		JSONObject fetchedObject= jsonBody.getJSONObject("Request");
-		
-		JSONArray getOptionalArr= fetchedObject.optJSONArray("NotificationList");
-		
+		JSONObject fetchedObject= jsonBody.getJSONObject(srhObj);//get the first Object value because jSon starting from jSon
+		JSONArray getOptionalArr= fetchedObject.optJSONArray(srhArray);//here i'm assume as array but get the key value  
+		JSONObject getNewObject = null; 
 		for(int i=0; i<getOptionalArr.length(); i++)
 		{
 			JSONObject getNextObj = getOptionalArr.getJSONObject(i);
 			
-			JSONArray newArray = getNextObj.getJSONArray("keyValuePairList");
+			JSONArray newArray = getNextObj.getJSONArray(srchArray);//get the next array which is under the previous  array
 				//System.out.println(newArray.toString());
 				
 				for(int j=0 ; j<newArray.length(); j++)
 				{
-					JSONObject getNewObject = newArray.getJSONObject(j);
+					 getNewObject = newArray.getJSONObject(j);
 					
-					if(getNewObject.get("key").equals("AmountDue"))
+					if(getNewObject.get(srchKey).equals(srchValu))// enter the key and value so get the expected data 
 					{
-						System.out.println(getNewObject.get("value").toString());
+						break; 
 					}
 				}
 		}
+		return  getNewObject.get(gtExtData).toString();
+	}
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		String expData= FetchDataMethodFromJson.getValueFromJson("Request", "NotificationList", "keyValuePairList", "key", "MobilePhoneNumber", "value");
 		
+		System.out.println(expData);
 	}
 
 }
